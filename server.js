8c43fe5ad10d3d58f8970c7588ff42ad409bf2d9
Brian Pruitt <brianrpruitt63@gmail.com>
2023-02-28 00:02:51 -0500

    fixed server config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen([PORT['0.0.0.0']], function () {
+app.listen(PORT, '0.0.0.0', function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
