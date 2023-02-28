de7719ef11197c77df6553bb71ee95059f3cdde4
Brian Pruitt <brianrpruitt63@gmail.com>
2023-02-27 23:27:47 -0500

    changed listen

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, function () {
+app.listen(`0.0.0.0:${PORT}`, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
